aef8695baf8b857a4a4be3151da74e79528cb205
CoderTymek <pazurtymek@gmail.com>
2021-06-09 23:39:52 +0200

    5th commit to my first project B)

---
+++ src/main/java/pl/coderslab/Repository/OfferRepository.java
@@ -0,0 +1,9 @@
+package pl.coderslab.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.model.Offer;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+}
